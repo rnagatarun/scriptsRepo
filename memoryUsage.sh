6ac26656044fa512a6e8177b0251e01dbb8222d7
NagaTarun <rnagatarun@gmail.com>
2024-12-22 18:32:05 +0530

    Memory usage script

--- memoryUsage.sh
+++ memoryUsage.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-
-# Log file variable
 logFile="memory_logFile.tsv"
 
-# Check if the log file exists
+# Email recipient
+recipient="rnagatarun@gmail.com"
+
 if [ ! -f "$logFile" ]; then
     # If the file doesn't exist, add a header
     echo "Date and Time                | Memory Usage (%)" > "$logFile"
@@ -11,26 +10,21 @@ if [ ! -f "$logFile" ]; then
 fi
 
 while true; do
-    # Get current memory usage
     memoryUsed=$(free -m | head -2 | tail -1 | awk '{print $3}')
     totalMemory=$(free -m | head -2 | tail -1 | awk '{print $2}')
     
-    # Calculate memory usage percentage
     memoryPercentageUsed=$((memoryUsed * 100 / totalMemory))
 
-    # Get current date and time
     dateTime=$(date '+%Y-%m-%d %H:%M:%S')
-
-    # Append the result to the log file
     echo "$dateTime | $memoryPercentageUsed%" >> "$logFile"
 
-    # Check if memory usage is greater than or equal to 80%
+    # Check if memory usage is greater than or equal to 80% for sending email
     if [[ $memoryPercentageUsed -ge 80 ]]; then
-        echo "Memory limit reached 80%" | espeak-ng
+        # Email notification
+        echo -e "Subject: Memory Usage Alert\n\nMemory usage has reached $memoryPercentageUsed% as of $dateTime." | mail -s "Memory Alert" "$recipient"
         exit
     fi
 
     # Sleep for 5 seconds
     sleep 5
 done
-
